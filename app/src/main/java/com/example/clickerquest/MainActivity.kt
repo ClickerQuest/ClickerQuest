f56de6c5dbc2721039d9dcb4fd932859bd7c616d
speedpick6 <81489476+speedpick6@users.noreply.github.com>
2022-03-25 18:56:33 -0400

    added fragments for each screen

--- app/src/main/java/com/example/clickerquest/MainActivity.kt
+++ app/src/main/java/com/example/clickerquest/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.clickerquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.clickerquest.fragments.HomeFragment
+import com.example.clickerquest.fragments.UpgradeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Test
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+
+            var fragmentToShow: Fragment? = null
+            when (item.itemId) {
+                R.id.active_home -> {
+                    fragmentToShow = HomeFragment()
+                }
+                R.id.active_Upgrade -> {
+                    fragmentToShow = UpgradeFragment()
+                }
+            }
+
+            if (fragmentToShow != null) {
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+            }
+            true
+        }
     }
+
+
 }
